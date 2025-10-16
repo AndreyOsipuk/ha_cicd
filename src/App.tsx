55c606c7e231f59f6e55652f5a2e387e2375ae89
Осипук <andrey.o@list.ru>
2025-10-16 19:29:24 +0300

    fix prefix

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Link, Route, Routes } from 'react-router'
 import { Main } from './components/main'
 import { Articals } from './components/acrticals'
 
+const local = false
+const prefix = local ? "" : "ha_cicd"
+
 function App() {
   return (
     <>
       <nav>
         <ul>
-          <li><Link to="/">Main</Link></li>
-          <li><Link to="articals">Articals</Link></li>
+          <li><Link to={`${prefix}/`}>Main</Link></li>
+          <li><Link to={`${prefix}/articals`}>Articals</Link></li>
         </ul>
       </nav>
 
